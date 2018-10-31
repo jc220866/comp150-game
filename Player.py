b6ee2effa314a82184d74f3ece82e02638032f3b
Cyrus Cleary <zeusofthecrows+github@gmail.com>
2018-10-31 13:17:45 +0000

    Smooth movement working and in the right place

--- Player.py
+++ Player.py
@@ -6,6 +6,7 @@ import ImageFiles
 
 class Player(pygame.sprite.Sprite):
     currentLane = 0  # 0: Middle, -1: Left, 1: Right
+    displaySurface = Helper.displaySurface
     playerSurf = ImageFiles.images['Player']
     playerRect = playerSurf.get_rect()
     playerPos = [Helper.RESOLUTION[0] * 0.5 - playerSurf.get_width() * 0.5,
@@ -35,22 +36,41 @@ class Player(pygame.sprite.Sprite):
         self.rect.x = self.rect.x + self.movex
         self.rect.y = self.rect.y + self.movey
 
-'''
     @staticmethod
-    def player_move(direction):  # needs four directions
-        if direction == 'move_right' and Player.currentLane < 1:
-            player_destination = Player.playerPos[0] + Player.moveDistance
-            Player.currentLane += 1
+    def player_action(action, player):
+        if 'move' in action:
+            Player.player_move(action, player)
+        elif 'idle' == action:
+            pass
 
-            while Player.playerPos[0] < player_destination:
-                Player.playerPos[0] += 5  # needs a loop in the "Main" script?
+    @staticmethod
+    def player_move(direction, player):  # needs four directions
+        """
+        Used for moving player upon swipe input, in future
+        will be used for moving from room to room also.
 
-        elif direction == 'move_left' and Player.currentLane > -1:
-            player_destination = Player.playerPos[0] - Player.moveDistance
-            Player.currentLane -= 1
+        Args:
+            direction -- string of the direction to move
+            player -- this player class
+        """
+        if direction == 'move_right' and player.currentLane < 1:
+            player_destination = player.playerPos[0] + player.moveDistance
+            player.currentLane += 1
 
-            while Player.playerPos[0] > player_destination:
-                Player.playerPos[0] -= 5  # needs a loop in the "Main" script?
-'''
+            while player.playerPos[0] < player_destination:
+                player.playerPos[0] += Helper.MOVE_SPEED  # needs a loop in the "Main" script?
+                Player.displaySurface.blit(ImageFiles.images['Background'], (0, 0))
+                Player.displaySurface.blit(player.image, (player.playerPos[0], player.playerPos[1]))
+                pygame.display.flip()
 
+        elif direction == 'move_left' and player.currentLane > -1:
+            player_destination = player.playerPos[0] - player.moveDistance
+            player.currentLane -= 1
+
+            while player.playerPos[0] > player_destination:
+                player.playerPos[0] -= Helper.MOVE_SPEED  # needs a loop in the "Main" script?
+                Player.displaySurface.blit(ImageFiles.images['Background'], (0, 0))
+                Player.displaySurface.blit(player.image, (player.playerPos[0], player.playerPos[1]))
+
+                pygame.display.flip()
 
