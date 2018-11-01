cf6877d4bfe31337841410bcc12e1aea94a3e955
Jakob <jc220866@falmouth.ac.uk>
2018-11-01 12:27:02 +0000

    Cleaned and fixed menu.py and Main.py's interaction with menu.py
    Highlighting works properly now
    Clicking 'continue' no longer crashes the program
    Buttons use a dictionary for maintainability

--- Menu.py
+++ Menu.py
@@ -11,76 +11,81 @@ pygame.display.set_caption('Sekai Saviour')
 BLACK = (0, 0, 0)                # currently used for the background
 BACKGROUND_GOLD = (124, 91, 51)   # currently unused
 BUTTON_GOLD = (147, 117, 53)      # colour of the buttons
-HIGHLIGHT = (150, 120, 100, 20)  # colour of the highlight, not sure how
-#  to get alpha working but we won't bother
+HIGHLIGHT = (150, 120, 100, 20)  # colour of the highlight, not sure how to get alpha working
 
-buttonNewGame = pygame.Rect(50, 600, 300, 150)
-buttonContinue = pygame.Rect(400, 600, 300, 150)
-buttonSettings = pygame.Rect(50, 800, 300, 150)
-buttonQuit = pygame.Rect(400, 800, 300, 150)
+buttons = dict(
+    buttonNewGame=pygame.Rect(50, 600, 300, 150),
+    buttonContinue=pygame.Rect(400, 600, 300, 150),
+    buttonSettings=pygame.Rect(50, 800, 300, 150),
+    buttonQuit=pygame.Rect(400, 800, 300, 150),
+    menuSettingsExit=pygame.Rect(25, 375, 700, 600),
+    menuExit=pygame.Rect(50, 900, 100, 50)
+    )
 
 
 def draw_menu():  # Draws rectangles to represent the 'buttons'
-    pygame.draw.rect(DISPLAY_SURFACE, BUTTON_GOLD, buttonNewGame)
-    pygame.draw.rect(DISPLAY_SURFACE, BUTTON_GOLD, buttonContinue)
-    pygame.draw.rect(DISPLAY_SURFACE, BUTTON_GOLD, buttonSettings)
-    pygame.draw.rect(DISPLAY_SURFACE, BUTTON_GOLD, buttonQuit)
+    pygame.draw.rect(DISPLAY_SURFACE, BUTTON_GOLD, buttons['buttonNewGame'])
+    pygame.draw.rect(DISPLAY_SURFACE, BUTTON_GOLD, buttons['buttonContinue'])
+    pygame.draw.rect(DISPLAY_SURFACE, BUTTON_GOLD, buttons['buttonSettings'])
+    pygame.draw.rect(DISPLAY_SURFACE, BUTTON_GOLD, buttons['buttonQuit'])
 
 
 def check_buttons(click_pos):
     """
     Checks which 'button' was clicked,
     can assign functions to each separate button
-    Args:
+    Arguments:
         click_pos -- position of the mouse click
     """
-    if buttonNewGame.collidepoint(click_pos):
+    if buttons['buttonNewGame'].collidepoint(click_pos):
         print("Button clicked: New Game")
         return 'New_Game'
-    if buttonContinue.collidepoint(click_pos):
+    if buttons['buttonContinue'].collidepoint(click_pos):
         print("Button clicked: Continue")
         # return 'Continue'
         pass
-    if buttonSettings.collidepoint(click_pos):
+    if buttons['buttonSettings'].collidepoint(click_pos):
         print("Button clicked: Settings")
         # return 'Settings'
         pass
-    if buttonQuit.collidepoint(click_pos):
+    if buttons['buttonQuit'].collidepoint(click_pos):
         print("Button clicked: Quit")
         # return 'Quit'
-        pass
+        return 'Quit'
     return 'Main_Menu'
 
 
 def highlight_buttons(mx, my):  # Draws a highlight over a button on mouse-over
-    if buttonNewGame.collidepoint(mx, my):
-        pygame.draw.rect(DISPLAY_SURFACE, HIGHLIGHT, buttonNewGame)
-    if buttonContinue.collidepoint(mx, my):
-        pygame.draw.rect(DISPLAY_SURFACE, HIGHLIGHT, buttonContinue)
-    if buttonSettings.collidepoint(mx, my):
-        pygame.draw.rect(DISPLAY_SURFACE, HIGHLIGHT, buttonSettings)
-    if buttonQuit.collidepoint(mx, my):
-        pygame.draw.rect(DISPLAY_SURFACE, HIGHLIGHT, buttonQuit)
+    if buttons['buttonNewGame'].collidepoint(mx, my):
+        pygame.draw.rect(DISPLAY_SURFACE, HIGHLIGHT, buttons['buttonNewGame'])
+    if buttons['buttonContinue'].collidepoint(mx, my):
+        pygame.draw.rect(DISPLAY_SURFACE, HIGHLIGHT, buttons['buttonContinue'])
+    if buttons['buttonSettings'].collidepoint(mx, my):
+        pygame.draw.rect(DISPLAY_SURFACE, HIGHLIGHT, buttons['buttonSettings'])
+    if buttons['buttonQuit'].collidepoint(mx, my):
+        pygame.draw.rect(DISPLAY_SURFACE, HIGHLIGHT, buttons['buttonQuit'])
 
 
 def menu_update():
+    (mouse_x, mouse_y) = (0, 0)
     while True:
         DISPLAY_SURFACE.fill(BLACK)
         draw_menu()
 
-        mouse_x = mouse_y = 0
-
         for event in pygame.event.get():
-            if event.type == QUIT:
+            # stores most recent mouse movement in two variables
+            if event.type == MOUSEMOTION:
+                mouse_x, mouse_y = event.pos
+
+            elif event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+
             elif event.type == KEYUP:
                 if event.key == K_ESCAPE:
                     pygame.quit()
                     sys.exit()
-            # stores most recent mouse movement in two variables
-            elif event.type == MOUSEMOTION:
-                mouse_x, mouse_y = event.pos
+
             # check if a button was clicked on mouse click
             elif event.type == MOUSEBUTTONUP:
                 click_pos = pygame.mouse.get_pos()
