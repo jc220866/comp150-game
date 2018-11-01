38f1d10b617abad10af6823fdf643c0496e1958f
Jakob <jc220866@falmouth.ac.uk>
2018-11-01 17:04:10 +0000

    Added enemy attack class

---
+++ EnemyAttacks.py
@@ -0,0 +1,35 @@
+import pygame
+import ImageFiles
+import Helper
+
+pygame.init()
+DISPLAY_SURFACE = Helper.DISPLAY_SURFACE
+
+attackSprites = []
+
+
+def enemy_attack(lane):
+
+    attackSprites.append(enemy_attack_sprite(lane))
+    pass
+
+
+class enemy_attack_sprite:
+
+    projectile_speed = 10  # Put in Helper at a later point
+
+    def __init__(self, lane):
+        self.sprite = ImageFiles.images['Enemy_Attack']
+        self.rect = self.sprite.get_rect()
+        self.lane = lane
+
+        self.pos_y = -10
+
+        if lane == 'MiddleLane':
+            self.pos_x = 375 - int(self.rect.width/2)
+
+    def update_position(self):
+
+        collision_with_player = False
+        while self.pos_y < 1334 and collision_with_player is False:
+            self.pos_y += enemy_attack_sprite.projectile_speed
